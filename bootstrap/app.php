1fe6ff86d28e09c13c4c625d0656fe8faa9fe8d6
Vlad06013 <vla-prokudi@yandex.ru>
2025-07-15 11:21:34 +0700

    refactor

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,13 +4,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         using: function () {
             Route::
-//            middleware('api/telegram-webapp/v1')
+            middleware('api')->
                 prefix('api/telegram-webapp/v1')
                 ->group(base_path('app/Domain/Clients/TelegramWebApp/routes/api.php'));
         },
@@ -22,6 +24,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->render(function (AccessDeniedHttpException $e) {
+            return response()->json(['error' => "Недостаточно прав."], Response::HTTP_UNAUTHORIZED);
+        });
         $exceptions->render(function (RuntimeException $e) {
             return response()->json(['error' => $e->getMessage()], $e->getCode());
         });
